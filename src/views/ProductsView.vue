63bf90b162d11b9d0feca4e348ef22588f70cbba
jaxhsu <bluepobo@gmail.com>
2022-04-10 20:57:54 +0800

    product update

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-end">
-    <button class="btn btn-primary" type="button" @click="$refs.ProductModal.showModal">
+    <button class="btn btn-primary" type="button" @click="openModal(true)">
       新增產品
     </button>
   </div>
@@ -17,26 +17,35 @@
     </thead>
     <tbody>
       <tr v-for="item in products" :key="item.id">
-        <td>{{item.category}}</td>
+        <td>{{ item.category }}</td>
         <!--分類-->
-        <td>{{item.title}}</td>
+        <td>{{ item.title }}</td>
         <!--標題-->
-        <td class="text-right">{{item.origin_price}}</td>
-        <td class="text-right">{{item.price}}</td>
+        <td class="text-right">{{ item.origin_price }}</td>
+        <td class="text-right">{{ item.price }}</td>
         <td>
           <span class="text-success" v-if="item.is_enabled">啟用</span>
           <span class="text-success" v-else>未啟用</span>
         </td>
         <td>
           <div class="btn-group">
-            <button class="btn btn-outline-primary btn-sm">編輯</button>
+            <button
+              class="btn btn-outline-primary btn-sm"
+              @click="openModal(false, item)"
+            >
+              編輯
+            </button>
             <button class="btn btn-outline-danger btn-sm">刪除</button>
           </div>
         </td>
       </tr>
     </tbody>
   </table>
-  <ProductModal ref="ProductModal"></ProductModal>
+  <ProductModal
+    ref="ProductModal"
+    :product="tempProduct"
+    @update-product="updateProduct"
+  ></ProductModal>
 </template>
 
 <script>
@@ -45,19 +54,51 @@ export default {
   data () {
     return {
       products: [],
-      pagination: {}
+      pagination: {},
+      tempProduct: {},
+      is_New: false
     };
   },
   components: {
     ProductModal
   },
   methods: {
+    openModal (isNew, item) {
+      const productComponent = this.$refs.ProductModal;
+      if (isNew) {
+        this.tempProduct = {};
+        this.is_New = true;
+      } else {
+        this.tempProduct = { ...item };
+        this.is_New = false;
+      }
+      productComponent.showModal();
+    },
     getProducts () {
-      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products`;
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/products`;
       console.log(api);
-      this.$http.get(api).then((res) => {
-        console.log(res);
-        this.products = res.data.products;
+      this.$http.get(api).then((response) => {
+        console.log(response);
+        this.products = response.data.products;
+      });
+    },
+    updateProduct (item) {
+      console.log('item=', item);
+      this.tempProduct = item;
+      // 新增
+      const productComponent = this.$refs.ProductModal;
+      let api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/product`;
+      let httpMethod = 'post';
+      // 編輯
+      if (!this.is_New) {
+        api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/product/${item.id}`;
+        httpMethod = 'put';
+      }
+
+      this.$http[httpMethod](api, { data: this.tempProduct }).then((response) => {
+        console.log(response);
+        productComponent.hideModal();
+        this.getProducts();
       });
     }
   },
